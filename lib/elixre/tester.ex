fe554cb00cc8a31e0c09b556e905ae9455242f65
Louis Pilfold <louis@lpil.uk>
2015-03-20 10:02:13 +0000

    Handle regex compilation errors

--- lib/elixre/tester.ex
+++ lib/elixre/tester.ex
@@ -3,11 +3,13 @@ defmodule Elixre.Tester do
   Takes a regex in string form, and one or a list of subject strings
   to run the regex on, and returns the results of the test.
 
+  If the regex is invalid it is returned with the errors.
+
   ## Examples
 
       iex> Elixre.Tester.test(~S".+ (\w+)", "Hello World!")
       %{
-        regex: ~S".+ (\w+)",
+        regex: ~r/.+ (\w+)/,
         results: [
           %{subject: "Hello World!", result: ["Hello World", "World"]}
         ]
@@ -15,27 +17,38 @@ defmodule Elixre.Tester do
 
       iex> Elixre.Tester.test("foo(.+)", ~w(foobar food foo))
       %{
-        regex: "foo(.+)",
+        regex: ~r/foo(.+)/,
         results: [
           %{subject: "foobar", result: ["foobar", "bar"]},
           %{subject: "food",   result: ["food", "d"]},
           %{subject: "foo",    result: nil}
         ]
       }
+
+      iex> Elixre.Tester.test("*foo", ~w(foobar food foo))
+      %{
+        error: {'nothing to repeat', 0}
+      }
   """
   def test(regex, subjects) when is_list(subjects) do
-    %{
-      regex: regex,
-      results: Enum.map(subjects, &result(regex, &1))
-    }
+    case Regex.compile(regex) do
+      {:ok, regex} ->
+        %{
+          regex: regex,
+          results: Enum.map(subjects, &result(regex, &1))
+        }
+
+      {:error, error} ->
+        %{
+          error: error
+        }
+    end
   end
   def test(regex, subject) do
     test(regex, [subject])
   end
 
   defp result(regex, subject) do
-    {:ok, regex} = Regex.compile(regex)
-
     %{
       subject: subject,
       result: Regex.run(regex, subject)
