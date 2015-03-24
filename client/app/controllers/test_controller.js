fc29fabe28a179bcfdc7e29cb7a13131e943b7a4
Louis Pilfold <louis@lpil.uk>
2015-03-24 21:29:46 +0000

    Refactor TestController

--- client/app/controllers/test_controller.js
+++ client/app/controllers/test_controller.js
@@ -1,23 +1,36 @@
 'use strict';
 
-class TestController {
-  constructor($scope, $http) {
-    $scope.test    = this.test;
-    $scope.http    = $http;
-    $scope.regex   = '';
-    $scope.subject = '';
-  }
+function TestController($scope, $http) {
 
-  test() {
-    var args = {
-      regex: this.regex,
-      subject: this.subject.split('\n')
+  /**
+   * Submit a regex and subject to the server for testing.
+   *
+   * Splits the subject on newline if $scope.split is true
+   *
+   * Side effect: Updates the UI to display result
+   *
+   * @param {string} regex - The regex to test
+   * @param {string} subject - The string on which to run the regex
+   */
+  const test = function test(regex, subject) {
+    if ($scope.split) { subject = subject.split('\n'); }
+
+    const args = {
+      regex: regex,
+      subject: subject
     };
-
-    this.http.post('/test', args)
+    $http.post('/test', args)
       .success(data => console.log(data))
       .error(data => console.log(data));
-  }
+  };
+
+
+  this.scope = $scope;
+
+  $scope.test    = test;
+  $scope.split   = true;
+  $scope.regex   = '';
+  $scope.subject = '';
 }
 
 export { TestController };
