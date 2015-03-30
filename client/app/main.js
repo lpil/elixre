024407d1ee35eb2bac4d4e83a08f6d7d04192032
Louis Pilfold <louis@lpil.uk>
2015-03-30 10:44:00 +0100

    Add empty results controller

--- client/app/main.js
+++ client/app/main.js
@@ -1,10 +1,12 @@
 require('angular');
 
 import { TestController } from './controllers/test_controller';
+import { ResultsController } from './controllers/results_controller';
 
 import { resultDirective } from './directives/result';
 
 angular
   .module('elixre', [])
   .controller('testController', TestController)
+  .controller('resultsController', ResultsController)
   .directive('result', resultDirective);
