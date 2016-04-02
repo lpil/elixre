e8f43b4d38439aac339faea5205a6e199e804fa6
Alice Amy Dee <amyalicedee@gmail.com>
2016-04-02 17:21:20 +0100

    Add router.

--- lib/elixre.ex
+++ lib/elixre.ex
@@ -2,4 +2,11 @@ defmodule Elixre do
   @moduledoc """
   Back end for Elixre.
   """
+  use Plug.Router
+  plug :match
+  plug :dispatch
+
+  get "/" do
+    send_resp(conn, 200, "hello dinosaur")
+  end
 end
--- test/elixre_test.exs
+++ test/elixre_test.exs
@@ -1,8 +1,17 @@
 defmodule ElixreTest do
   use ExUnit.Case
+  use Plug.Test
   doctest Elixre
+  
+  @opts Elixre.init []
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "test home route" do
+    conn =
+      :get 
+      |> conn("/")
+      |> Elixre.call(@opts)
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "hello dinosaur"
   end
 end
