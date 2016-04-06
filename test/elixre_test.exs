c0ab0b7943032f082929b06733a8a76219c4141a
Louis Pilfold <louis@lpil.uk>
2016-04-06 08:53:08 +0100

    Pending /regex json test

--- test/elixre_test.exs
+++ test/elixre_test.exs
@@ -25,4 +25,17 @@ defmodule ElixreTest do
     assert conn.status == 404
     assert conn.resp_body == "Page not found"
   end
+
+  @tag :skip
+  test "/regex" do
+    conn =
+      :post
+      |> conn("/regex", "[1,2,3,4]")
+      |> put_req_header("content-type", "application/json")
+      |> Elixre.call(@opts)
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "[4,3,2,1]"
+    assert "application/json" in get_resp_header(conn, "content-type")
+  end
 end
