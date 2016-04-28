30f4e7a3f058009cfb7f80729f1b23e41bac20a1
Louis Pilfold <louis@lpil.uk>
2016-04-28 19:55:28 +0100

    Add pending tests for API parram validation

--- test/api_test.exs
+++ test/api_test.exs
@@ -12,11 +12,92 @@ defmodule Elixre.APITest do
     |> Elixre.call(@opts)
   end
 
-  test "/regex" do
-    conn = send_to_api([1, 2, 3, 4])
+  defp assert_body(conn, data) do
+    body = conn.resp_body |> Poison.decode!
+    expected = data |> Poison.encode!
+    assert body == expected
+  end
+
+  defp assert_header(conn, header, pattern) do
+    header = conn |> get_resp_header(header) |> hd
+    assert header =~ pattern
+  end
+
+
+  @tag :skip
+  test "a valid request" do
+    conn = send_to_api(%{
+      regex: %{
+        pattern: "foo",
+        modifiers: "iu",
+        subjects: ["foobarfoo", "FOOBAR", "barfoo"],
+      },
+    })
     assert conn.status == 200
-    assert conn.resp_body == "[4,3,2,1]"
-    [header] = get_resp_header(conn, "content-type")
-    assert header =~ "application/json"
+    assert_header(conn, "content-type", "application/json")
+    assert_body(conn, %{
+      regex: %{
+        pattern: "^foo",
+        modifiers: "iu",
+        results: [
+          %{ subject: "foobar" },
+          %{ subject: "FOOBAR" },
+          %{ subject: "barfoo" },
+        ],
+      },
+    })
+  end
+
+  @tag :skip
+  test "a valid request (no modifiers param)" do
+    conn = send_to_api(%{
+      regex: %{
+        pattern: "foo",
+        subjects: ["foobarfoo", "barfoo"],
+      },
+    })
+    assert conn.status == 200
+    assert_body(conn, %{
+      regex: %{
+        pattern: "^foo",
+        modifiers: "",
+        results: [
+          %{ subject: "foobar" },
+          %{ subject: "barfoo" },
+        ],
+      },
+    })
+  end
+
+  @tag :skip
+  test "a valid request (no subjects param)" do
+    conn = send_to_api(%{
+      regex: %{
+        pattern: "foo",
+        modifiers: "i",
+      },
+    })
+    assert conn.status == 200
+    assert_body(conn, %{
+      regex: %{
+        pattern: "^foo",
+        modifiers: "",
+        results: [],
+      },
+    })
+  end
+
+  @tag :skip
+  test "missing `pattern` param" do
+    conn = send_to_api(%{
+      regex: %{
+        modifiers: "iu",
+        subjects: ["foobarfoo", "FOOBAR", "barfoo"],
+      },
+    })
+    assert conn.status == 400
+    assert_body(conn, %{
+      errors: ["`pattern` parameter must be present"],
+    })
   end
 end
