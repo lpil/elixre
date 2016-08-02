86be8fafb0e781135c0a73546160dd2e3a165675
Louis Pilfold <louis@lpil.uk>
2016-08-02 09:08:23 +0100

    Input.View

---
+++ client/Input/View.elm
@@ -0,0 +1,35 @@
+module Input.View exposing (root)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+root : Html a
+root =
+    div [ class "inputs" ]
+        [ div [] (pattern ++ modifiers)
+        , div [] subject
+        ]
+
+
+pattern : List (Html a)
+pattern =
+    [ label [ for "regex", class "regex-label" ] [ text "Regex" ]
+    , input [ name "regex", class "regex-input", autofocus True ] []
+    ]
+
+
+modifiers : List (Html a)
+modifiers =
+    [ label [ for "modifiers", class "modifiers-label" ] [ text "Modifiers" ]
+    , input [ name "modifiers", class "regex-input", autofocus True ] []
+    ]
+
+
+subject : List (Html a)
+subject =
+    [ label [ for "subject" ] [ text "Subject" ]
+    , textarea [ name "subject", rows 5 ] []
+    , label [ for "split", class "subject-split" ] [ text "Split on newlines?" ]
+    , input [ name "split", type' "checkbox" ] []
+    ]
--- client/Main.elm
+++ client/Main.elm
@@ -1,94 +1,44 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (..)
 import Html.App as Html
-import Html.Events exposing (onClick)
-
-
--- component import example
-
-import Components.Hello exposing (hello)
-
-
--- APP
+import Input.View
 
 
 main : Program Never
 main =
-    Html.beginnerProgram { model = model, view = view, update = update }
-
-
-
--- MODEL
+    Html.program
+        { init = init
+        , view = view
+        , subscriptions = subscriptions
+        , update = update
+        }
 
 
 type alias Model =
     Int
 
 
-model : number
-model =
-    0
+init : ( Model, Cmd Msg )
+init =
+    ( 0, Cmd.none )
 
 
-
--- UPDATE
+subscriptions : Model -> Sub a
+subscriptions model =
+    Sub.none
 
 
 type Msg
     = NoOp
-    | Increment
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        NoOp ->
-            model
-
-        Increment ->
-            model + 1
+    ( model, Cmd.none )
 
 
-
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
-
-
-view : Model -> Html Msg
 view model =
-    div [ class "container", style [ ( "margin-top", "30px" ), ( "text-align", "center" ) ] ]
-        [ -- inline CSS (literal)
-          div [ class "row" ]
-            [ div [ class "col-xs-12" ]
-                [ div [ class "jumbotron" ]
-                    [ img [ src "", style styles.img ] []
-                      -- inline CSS (via var)
-                    , hello model
-                      -- ext 'hello' component (takes 'model' as arg)
-                    , p [] [ text ("Elm Webpack Starter") ]
-                    , button [ class "btn btn-primary btn-lg", onClick Increment ]
-                        [ -- click handler
-                          span [ class "glyphicon glyphicon-star" ] []
-                          -- glyphicon
-                        , span [] [ text "FTW!" ]
-                        ]
-                    ]
-                ]
-            ]
+    div []
+        [ Input.View.root
         ]
-
-
-
--- CSS STYLES
-
-
-styles : { img : List ( String, String ) }
-styles =
-    { img =
-        [ ( "width", "33%" )
-        , ( "border", "4px solid #337AB7" )
-        ]
-    }
